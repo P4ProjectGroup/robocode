78df1f916f54119426b37303af9aab1a6d59f791
Martijn de Bijl <martijn.debijl@gmail.com>
2018-06-29 22:36:17 +0200

    Perhaps this is the solution?

--- robocode.api/src/main/java/robocode/util/Utils.java
+++ robocode.api/src/main/java/robocode/util/Utils.java
@@ -39,9 +39,9 @@ public class Utils {
 	 * @return the normalized angle that will be in the range of [0,2*PI[
 	 */
 	public static double normalAbsoluteAngle(double angle) {
-		double result = angle + TWO_PI;
+		double result = ( angle % TWO_PI ) + TWO_PI;
 		if ( angle % TWO_PI >= 0 ) {
-			result = angle;	
+			result = angle % TWO_PI;	
 		}
 		
 		return result;
